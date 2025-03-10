c715bde55ea75f94fe2a77b950b90c33cc738978
MINEDUCYT <MINEDUCYT@DESKTOP-CC30LNJ>
2025-03-09 21:05:31 -0600

    Agregar archivos de proyecto.

---
+++ PruebaDatafirst0309/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PruebaDatafirst0309.Models;
+
+namespace PruebaDatafirst0309.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ PruebaDatafirst0309/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaDatafirst0309.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
