f22862a638d7262bf4b8958e7b9c31a9931d53d1
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-03-10 20:37:37 -0600

    code first

---
+++ PruebaCodefirst/Models/MiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PruebaCodefirst.Models
+{
+    public class MiContext: DbContext
+    {
+
+        public MiContext(DbContextOptions<MiContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+
+
+
+    }
+}
